3afa957e2c4875a576212cbd7f480c8a8c94c9cc
OptaxDev <optax-dev@google.com>
2020-10-05 02:55:20 -0700

    A function that wraps an optimiser to make it robust to a few NaNs or Infs.
    PiperOrigin-RevId: 335377127
    Change-Id: I50c71c7db3f6e3d4ffe3d1f71ff20f227bb50a03

--- optax/_src/wrappers.py
+++ optax/_src/wrappers.py
@@ -15,8 +15,12 @@
 # ==============================================================================
 """Transformation wrappers."""
 
+from typing import Any, NamedTuple
+
+from jax import lax
 import jax.numpy as jnp
 from jax.tree_util import tree_flatten
+from jax.tree_util import tree_map
 from jax.tree_util import tree_unflatten
 import numpy as np
 
@@ -73,3 +77,77 @@ def flatten(
     return updates, state
 
   return transform.GradientTransformation(init_fn, update_fn)
+
+
+class ApplyIfFiniteState(NamedTuple):
+  """State of the `GradientTransformation` returned by `apply_if_finite`.
+
+  Fields:
+    notfinite_count: Number of consecutive gradient updates containing an Inf or
+      a NaN. This number is reset to 0 whenever a gradient update without an Inf
+      or a NaN is done.
+    last_finite: Whether or not the last gradient update contained an Inf of a
+      NaN.
+    total_notfinite: Total number of gradient updates containing an Inf or
+      a NaN since this optimiser was initialised. This number is never reset.
+    inner_state: The state of the inner `GradientTransformation`.
+  """
+  notfinite_count: jnp.array
+  last_finite: jnp.array
+  total_notfinite: jnp.array
+  inner_state: Any
+
+
+def apply_if_finite(
+    inner: transform.GradientTransformation,
+    max_consecutive_errors: int
+) -> transform.GradientTransformation:
+  """A function that wraps an optimiser to make it robust to a few NaNs or Infs.
+
+  The purpose of this function is to prevent any optimisation to happen if the
+  gradients contain NaNs or Infs. That is, when a NaN of Inf is detected in the
+  gradients, the wrapped optimiser ignores that gradient update. If the NaNs or
+  Infs persist after a given number of updates, the wrapped optimiser gives up
+  and accepts the update.
+
+  Args:
+    inner: Inner transformation to be wrapped.
+    max_consecutive_errors: Maximum number of consecutive gradient updates
+      containing NaNs of Infs that the wrapped optimiser will ignore. After
+      that many ignored updates, the optimiser will give up and accept.
+
+  Returns:
+    New GradientTransformation.
+  """
+
+  def init(params):
+    return ApplyIfFiniteState(
+        notfinite_count=jnp.zeros([], jnp.int64),
+        last_finite=jnp.array(True, jnp.bool_),
+        total_notfinite=jnp.zeros([], jnp.int64),
+        inner_state=inner.init(params))
+
+  def update(grads, state, params=None):
+    inner_state = state.inner_state
+    flat_grads = tree_flatten(grads)[0]
+    isfinite = jnp.all(
+        jnp.array([jnp.all(jnp.isfinite(p)) for p in flat_grads]))
+    notfinite_count = jnp.where(isfinite, jnp.zeros([], jnp.int64),
+                                1 + state.notfinite_count)
+
+    def do_update(_):
+      return inner.update(grads, inner_state, params)
+    def reject_update(_):
+      return (tree_map(jnp.zeros_like, grads), inner_state)
+
+    updates, new_inner_state = lax.cond(
+        jnp.logical_or(isfinite, notfinite_count > max_consecutive_errors),
+        do_update, reject_update, operand=None)
+
+    return updates, ApplyIfFiniteState(
+        notfinite_count=notfinite_count,
+        last_finite=isfinite,
+        total_notfinite=jnp.logical_not(isfinite) + state.total_notfinite,
+        inner_state=new_inner_state)
+
+  return transform.GradientTransformation(init=init, update=update)
--- optax/_src/wrappers_test.py
+++ optax/_src/wrappers_test.py
@@ -16,14 +16,26 @@
 """Tests for `wrappers.py`."""
 
 from absl.testing import absltest
+from absl.testing import parameterized
 import chex
+import haiku as hk
+import jax
 import jax.numpy as jnp
 from optax._src import alias
 from optax._src import update
 from optax._src import wrappers
 
 
-class WrappersTest(chex.TestCase):
+def _build_sgd():
+  return alias.sgd(1.)
+
+
+def _build_simple_adam():
+  # This adam behaves like an sgd, but with state.
+  return alias.adam(1., b1=0., b2=0.)
+
+
+class WrappersTest(parameterized.TestCase):
 
   def test_flatten(self):
     def init_params():
@@ -49,6 +61,102 @@ class WrappersTest(chex.TestCase):
     chex.assert_tree_all_close(
         sgd_params_no_flatten, sgd_params_flatten, atol=1e-7, rtol=1e-7)
 
+  @chex.variants(with_jit=True, without_jit=True, with_pmap=True)
+  @parameterized.parameters([
+      _build_sgd,
+      _build_simple_adam,
+  ])
+  def test_apply_if_finite(self, opt_builder):
+    one = jnp.ones([])
+    nan = jnp.array(jnp.nan)
+    def fn(x):
+      return x * hk.get_parameter('p', [], init=hk.initializers.Constant(0.))
+
+    fn = hk.without_apply_rng(hk.transform(fn))
+    params = fn.init(jax.random.PRNGKey(1905), one)
+    opt = wrappers.apply_if_finite(opt_builder(), 2)
+    state = opt.init(params)
+    grads_fn = jax.grad(self.variant(fn.apply))
+    # Do one successful param update
+    grads = grads_fn(params, one)
+    updates, state = opt.update(grads, state, params)
+    params = update.apply_updates(params, updates)
+    # We know exactly what should be the value of params since we are
+    # effectively using sgd in all cases.
+    self.assertEqual(-1., float(jax.tree_flatten(params)[0][0]))
+    self.assertTrue(bool(state.last_finite))
+    # Check 2 rejected param updates
+    for step in range(2):
+      grads = grads_fn(params, nan)
+      updates, state = opt.update(grads, state, params)
+      params = update.apply_updates(params, updates)
+      self.assertEqual(-1., float(jax.tree_flatten(params)[0][0]))
+      self.assertFalse(bool(state.last_finite))
+      self.assertEqual(step + 1, int(state.notfinite_count))
+    # Next successful param update
+    grads = grads_fn(params, one)
+    updates, state = opt.update(grads, state, params)
+    params = update.apply_updates(params, updates)
+    self.assertEqual(-2., float(jax.tree_flatten(params)[0][0]))
+    self.assertTrue(bool(state.last_finite))
+    # Again 2 rejected param updates
+    for step in range(2):
+      grads = grads_fn(params, nan)
+      updates, state = opt.update(grads, state, params)
+      params = update.apply_updates(params, updates)
+      self.assertEqual(-2., float(jax.tree_flatten(params)[0][0]))
+      self.assertFalse(bool(state.last_finite))
+      self.assertEqual(step + 1, int(state.notfinite_count))
+    # Next param update with NaN is accepted since we reached maximum
+    grads = grads_fn(params, nan)
+    updates, state = opt.update(grads, state, params)
+    params = update.apply_updates(params, updates)
+    self.assertTrue(bool(jnp.isnan(jax.tree_flatten(params)[0][0])))
+    self.assertEqual(5, int(state.total_notfinite))
+
+  def test_apply_if_finite_pmap(self):
+    # Unlike in `test_apply_if_finite`:
+    # * pmap is applied to the gradient computation and the optimisation;
+    # * the NaNs are caused inside the function and do not come from the inputs.
+    half = jnp.ones([1]) / 2.
+    two = jnp.ones([1]) * 2.  # Causes a NaN in arctanh
+    def fn(x):
+      return jnp.arctanh(x) * hk.get_parameter(
+          'p', [], init=hk.initializers.Constant(0.))
+    fn = hk.without_apply_rng(hk.transform(fn))
+
+    opt = wrappers.apply_if_finite(alias.sgd(1.), 2)
+    def fn_update(params, opt_state, x):
+      grads = jax.grad(fn.apply)(params, x)
+      grads = jax.lax.psum(grads, axis_name='i')
+      updates, new_opt_state = opt.update(grads, opt_state, params)
+      new_params = update.apply_updates(params, updates)
+      return new_params, new_opt_state
+    fn_update = jax.pmap(fn_update, axis_name='i')
+
+    params = fn.init(jax.random.PRNGKey(1905), half)
+    opt_state = opt.init(params)
+    params = jax.tree_map(lambda x: x[None], params)
+    opt_state = jax.tree_map(lambda x: x[None], opt_state)
+    # Do one successful param update
+    params, opt_state = fn_update(params, opt_state, half)
+    self.assertTrue(bool(opt_state.last_finite))
+    # Check 2 rejected param updates
+    for step in range(2):
+      params, opt_state = fn_update(params, opt_state, two)
+      self.assertFalse(bool(opt_state.last_finite))
+      self.assertEqual(step + 1, int(opt_state.notfinite_count))
+    # Next successful param update
+    params, opt_state = fn_update(params, opt_state, half)
+    self.assertTrue(bool(opt_state.last_finite))
+    # Again 2 rejected param updates
+    for step in range(2):
+      params, opt_state = fn_update(params, opt_state, two)
+      self.assertFalse(bool(opt_state.last_finite))
+      self.assertEqual(step + 1, int(opt_state.notfinite_count))
+    # Next param update with NaN is accepted since we reached maximum
+    params, opt_state = fn_update(params, opt_state, two)
+    self.assertEqual(5, int(opt_state.total_notfinite))
 
 if __name__ == '__main__':
   absltest.main()
