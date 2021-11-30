97f526860844092542c6de987b80467558c8a40f
Dian Wu <dian.wu@epfl.ch>
2021-11-30 20:04:42 +0100

    Add a test where params is not None

--- optax/_src/complex_valued_test.py
+++ optax/_src/complex_valued_test.py
@@ -15,14 +15,15 @@
 """Tests for `complex_valued.py`."""
 
 from absl.testing import absltest
+from absl.testing import parameterized
 
 import chex
 import jax
 import jax.numpy as jnp
 import numpy as np
 
-from optax._src import alias
 from optax._src import complex_valued
+from optax._src import transform
 from optax._src import update
 
 
@@ -35,16 +36,21 @@ def _loss_fun_real_to_real(params):
   return _loss_fun_complex_to_real(x + y * 1j)
 
 
-class ComplexValuedTest(chex.TestCase):
+class ComplexValuedTest(parameterized.TestCase):
 
   @chex.all_variants
-  def test_split_complex(self):
+  @parameterized.named_parameters([
+      ('adam', transform.scale_by_adam),
+      ('param_block_norm', transform.scale_by_param_block_norm),
+  ])
+  def test_split_complex(self, scaler_constr):
 
     def do_update(loss_fun, optimizer, params, opt_state):
       loss, grads = jax.value_and_grad(loss_fun)(params)
       # Complex gradients need to be conjugated before being added to parameters
       grads = jax.tree_map(lambda x: x.conj(), grads)
-      updates, opt_state = self.variant(optimizer.update)(grads, opt_state)
+      updates, opt_state = self.variant(optimizer.update)(
+          grads, opt_state, params)
       params = update.apply_updates(params, updates)
       return loss, grads, params, opt_state
 
@@ -53,7 +59,7 @@ class ComplexValuedTest(chex.TestCase):
     y = jnp.asarray(np.random.randn(3))
     z = x + y * 1j
 
-    optimizer = alias.adam(learning_rate=1e-2)
+    optimizer = scaler_constr()
     optimizer_complex = complex_valued.split_complex(optimizer)
     opt_state = self.variant(optimizer.init)((x, y))
     opt_state_complex = self.variant(optimizer_complex.init)(z)
@@ -65,9 +71,9 @@ class ComplexValuedTest(chex.TestCase):
           _loss_fun_real_to_real, optimizer, (x, y), opt_state)
       loss_complex, gz, z, opt_state_complex = do_update(
           _loss_fun_complex_to_real, optimizer_complex, z, opt_state_complex)
-      np.testing.assert_allclose(loss, loss_complex)
-      np.testing.assert_allclose(gx + gy * 1j, gz)
-      np.testing.assert_allclose(x + y * 1j, z)
+      np.testing.assert_allclose(loss, loss_complex, rtol=1e-6, atol=1e-6)
+      np.testing.assert_allclose(gx + gy * 1j, gz, rtol=1e-6, atol=1e-6)
+      np.testing.assert_allclose(x + y * 1j, z, rtol=1e-6, atol=1e-6)
 
 
 if __name__ == '__main__':
