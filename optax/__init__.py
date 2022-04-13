123ba040eb3989dd0f61344b7249766daa833bd6
Iurii Kemaev <iukemaev@google.com>
2022-04-13 03:18:13 -0700

    Release v0.1.2
    PiperOrigin-RevId: 441427965

--- optax/__init__.py
+++ optax/__init__.py
@@ -154,7 +154,7 @@ from optax._src.wrappers import MaybeUpdateState
 from optax._src.wrappers import MultiSteps
 from optax._src.wrappers import MultiStepsState
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 __all__ = (
     "adabelief",
