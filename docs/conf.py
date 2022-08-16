2d7d6fe1720774283a076db897f87960b7366aaf
Ross Hemsley <rhemsley@google.com>
2022-08-16 03:15:58 -0700

    Update logo and theme for the documentation
    PiperOrigin-RevId: 467881724

--- docs/conf.py
+++ docs/conf.py
@@ -142,7 +142,7 @@ extensions = [
     'sphinxcontrib.bibtex',
     'sphinxcontrib.katex',
     'sphinx_autodoc_typehints',
-    'sphinx_rtd_theme',
+    'sphinx_book_theme',
     'coverage_check',
     'myst_nb',  # This is used for the .ipynb notebooks
 ]
@@ -163,18 +163,28 @@ autodoc_default_options = {
     'exclude-members': '__repr__, __str__, __weakref__',
 }
 
+# -- Options for bibtex ------------------------------------------------------
+
+bibtex_bibfiles = []
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_book_theme'
+
+html_theme_options = {
+    'logo_only': True,
+    'show_toc_level': 2,
+}
+
+html_logo = 'images/logo.svg'
+html_favicon = 'images/favicon.svg'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = []
-# html_favicon = '_static/favicon.ico'
 
 # -- Options for myst -------------------------------------------------------
 
