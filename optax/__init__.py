296094af24909c9c03c4a3701578b5689ef4bcea
wglao <wesleygarlao@gmail.com>
2023-01-25 11:16:50 -0600

    correct version name

--- optax/__init__.py
+++ optax/__init__.py
@@ -180,7 +180,7 @@ from optax._src.wrappers import ShouldSkipUpdateFunction
 from optax._src.wrappers import skip_large_updates
 from optax._src.wrappers import skip_not_finite
 
-__version__ = "0.1.5.dev"
+__version__ = "0.1.5.dev0"
 
 __all__ = (
     "adabelief",
