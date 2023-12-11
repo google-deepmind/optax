8facf45155ae9055b801efe1d7114fbc635861d7
vz415 <19888961+vz415@users.noreply.github.com>
2023-12-11 14:17:01 -0600

    fixed spacing for ReduceLROnPlateau

--- optax/contrib/reduce_on_plateau_test.py
+++ optax/contrib/reduce_on_plateau_test.py
@@ -15,92 +15,96 @@
 """Tests for `reduce_on_plateau.py`."""
 
 from absl.testing import absltest
-import chex
-import jax
-import jax.numpy as jnp
 
-from optax._src import base
-from optax._src import transform
 from optax.contrib.reduce_on_plateau import  ReduceLROnPlateauState, reduce_on_plateau
 
 
 class ReduceLROnPlateauTest(absltest.TestCase):
-    def test_learning_rate_reduced_after_cooldown_period_is_over(self):
-        """Test that learning rate is reduced again after cooldown period is over."""
-        # Define a state where cooldown_counter is zero
-        state = ReduceLROnPlateauState(
-            reduce_factor=0.5,
-            patience=5,
-            min_improvement=1e-4,
-            best_loss=0.1,
-            ##
-            plateau_count=4,
-            ##
-            lr=0.01,
-            cooldown=5,
-            cooldown_counter=0,
-        )
-        # Define a dummy update and extra_args
-        updates = {'params': 1}
-        extra_args = {'loss': 0.15}
-        # Apply the transformation to the updates and state
-        transform = reduce_on_plateau(reduce_factor=0.5, patience=5, min_improvement=1e-4, cooldown=5)
-        new_updates, new_state = transform.update(updates=updates, state=state, extra_args=extra_args)
-        # Check that learning rate is reduced
-        assert new_state.lr == 0.005
-        assert new_state.plateau_count == 0
-        assert new_state.cooldown_counter == 5
-        new_updates, new_state = transform.update(updates=updates, state=new_state, extra_args=extra_args)
-        assert new_state.lr == 0.005
-        assert new_state.plateau_count == 0
-        assert new_state.cooldown_counter == 4
+  def test_learning_rate_reduced_after_cooldown_period_is_over(self):
+    """Test that learning rate is reduced again after cooldown period is over.
+    """
+    # Define a state where cooldown_counter is zero
+    state = ReduceLROnPlateauState(
+      reduce_factor=0.5,
+      patience=5,
+      min_improvement=1e-4,
+      best_loss=0.1,
+      ##
+      plateau_count=4,
+      ##
+      lr=0.01,
+      cooldown=5,
+      cooldown_counter=0,
+    )
+    # Define a dummy update and extra_args
+    updates = {'params': 1}
+    extra_args = {'loss': 0.15}
+    # Apply the transformation to the updates and state
+    transform = reduce_on_plateau(
+      reduce_factor=0.5, patience=5, min_improvement=1e-4, cooldown=5)
+    _, new_state = transform.update(
+      updates=updates, state=state, extra_args=extra_args)
+    # Check that learning rate is reduced
+    assert new_state.lr == 0.005
+    assert new_state.plateau_count == 0
+    assert new_state.cooldown_counter == 5
+    _, new_state = transform.update(
+      updates=updates, state=new_state, extra_args=extra_args)
+    assert new_state.lr == 0.005
+    assert new_state.plateau_count == 0
+    assert new_state.cooldown_counter == 4
 
 
-    def test_learning_rate_is_not_reduced(self):
-        """Test that plateau count resets after a new best loss is found."""
-        state = ReduceLROnPlateauState(
-            reduce_factor=0.5,
-            patience=5,
-            min_improvement=1e-4,
-            best_loss=0.1,
-            plateau_count=3,
-            lr=0.01,
-            cooldown_counter=0,
-            cooldown=5,
-        )
-        # Define a dummy update and extra_args
-        updates = {'params': 1}
-        extra_args = {'loss': 0.01}
-        # Apply the transformation to the updates and state
-        transform = reduce_on_plateau(reduce_factor=0.5, patience=5, min_improvement=1e-4, cooldown=5)
-        new_updates, new_state = transform.update(updates=updates, state=state, extra_args=extra_args)
-        # Check that plateau count resets
-        assert new_state.plateau_count == 0
-        assert new_state.best_loss == 0.01
+  def test_learning_rate_is_not_reduced(self):
+    """Test that plateau count resets after a new best loss is found."""
+    state = ReduceLROnPlateauState(
+      reduce_factor=0.5,
+      patience=5,
+      min_improvement=1e-4,
+      best_loss=0.1,
+      plateau_count=3,
+      lr=0.01,
+      cooldown_counter=0,
+      cooldown=5,
+    )
+    # Define a dummy update and extra_args
+    updates = {'params': 1}
+    extra_args = {'loss': 0.01}
+    # Apply the transformation to the updates and state
+    transform = reduce_on_plateau(
+      reduce_factor=0.5, patience=5, min_improvement=1e-4, cooldown=5)
+    _, new_state = transform.update(
+      updates=updates, state=state, extra_args=extra_args)
+    # Check that plateau count resets
+    assert new_state.plateau_count == 0
+    assert new_state.best_loss == 0.01
 
 
-    def test_learning_rate_not_reduced_during_cooldown(self):
-        """Test that learning rate is not reduced during cooldown."""
-        # Define a state where cooldown_counter is positive
-        state = ReduceLROnPlateauState(
-            reduce_factor=0.5,
-            patience=5,
-            min_improvement=1e-4,
-            best_loss=0.1,
-            plateau_count=4,
-            lr=0.01,
-            cooldown=5,
-            cooldown_counter=3,
-        )
-        # Define a dummy update and extra_args
-        updates = {'params': 1}
-        extra_args = {'loss': 0.15}
-        # Apply the transformation to the updates and state
-        transform = reduce_on_plateau(reduce_factor=0.5, patience=5, min_improvement=1e-4, cooldown=5)
-        new_updates, new_state = transform.update(updates=updates, state=state, extra_args=extra_args)
-        # Check that learning rate is not reduced
-        assert new_state.lr == 0.01
+  def test_learning_rate_not_reduced_during_cooldown(self):
+    """Test that learning rate is not reduced during cooldown."""
+    # Define a state where cooldown_counter is positive
+    state = ReduceLROnPlateauState(
+      reduce_factor=0.5,
+      patience=5,
+      min_improvement=1e-4,
+      best_loss=0.1,
+      plateau_count=4,
+      lr=0.01,
+      cooldown=5,
+      cooldown_counter=3,
+    )
+    # Define a dummy update and extra_args
+    updates = {'params': 1}
+    extra_args = {'loss': 0.15}
+    # Apply the transformation to the updates and state
+    transform = reduce_on_plateau(
+      reduce_factor=0.5, patience=5, min_improvement=1e-4, cooldown=5)
+    _, new_state = transform.update(
+      updates=updates, state=state, extra_args=extra_args)
+    # Check that learning rate is not reduced
+    assert new_state.lr == 0.01
 
 
 if __name__ == '__main__':
-    absltest.main()
+  absltest.main()
+  
