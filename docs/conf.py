5a999ca85cf1163db0e60e9b46aad7af62f9f9ab
Amr Khalifa <amrkhalifa@google.com>
2024-02-05 07:05:31 -0800

    fixed math flex direction
    PiperOrigin-RevId: 604306995

--- docs/conf.py
+++ docs/conf.py
@@ -211,7 +211,11 @@ html_favicon = 'images/favicon.svg'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+
+html_static_path = ['_static']
+html_css_files = [
+    'css/custom.css',
+]
 
 # -- Options for myst -------------------------------------------------------
 nb_execution_mode = 'force'
