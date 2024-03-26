041f22b714e0eec429fb5a692ef1f72f66594838
Fabian Schaipp <fabian.schaipp@hotmail.de>
2024-03-26 15:06:54 +0100

    remove common test for now

--- optax/contrib/_common_test.py
+++ optax/contrib/_common_test.py
@@ -34,8 +34,6 @@ _OPTIMIZERS_UNDER_TEST = (
     dict(opt_name='cocob', opt_kwargs=dict(alpha=100.0, eps=1e-8)),
     dict(opt_name='dadapt_adamw', opt_kwargs=dict(learning_rate=1e-1)),
     dict(opt_name='prodigy', opt_kwargs=dict(learning_rate=1e-1)),
-    dict(opt_name='momo', opt_kwargs=dict(learning_rate=1e-1)),
-    dict(opt_name='momo_adam', opt_kwargs=dict(learning_rate=1e-1)),
 )
 
 
