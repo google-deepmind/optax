49c1cffcbb1e9c612cb79ca2a30a66b4e14a479a
Vincent Roulet <vroulet@google.com>
2024-04-22 12:31:24 -0700

    Silence installs to improve readability of tests.
    PiperOrigin-RevId: 627122107

--- test.sh
+++ test.sh
@@ -27,22 +27,22 @@ source "${VENV_DIR}/bin/activate"
 python --version
 
 # Install dependencies.
-pip install --upgrade pip setuptools wheel
-pip install flake8 pytest-xdist pylint pylint-exit
-pip install -e ".[test, examples]"
+pip install -q --upgrade pip setuptools wheel
+pip install -q flake8 pytest-xdist pylint pylint-exit
+pip install -q -e ".[test, examples]"
 
 # Dp-accounting specifies exact minor versions as requirements which sometimes
 # become incompatible with other libraries optax needs. We therefore install
 # dependencies for dp-accounting manually.
 # TODO(b/239416992): Remove this workaround if dp-accounting switches to minimum
 # version requirements.
-pip install -e ".[dp-accounting]"
-pip install "dp-accounting>=0.1.1" --no-deps
+pip install -q -e ".[dp-accounting]"
+pip install -q "dp-accounting>=0.1.1" --no-deps
 
 # Ensure optax was not installed by one of the dependencies above,
 # since if it is, the tests below will be run against that version instead of
 # the branch build.
-pip uninstall -y optax || true
+pip uninstall -q -y optax || true
 
 # Lint with flake8.
 flake8 `find optax examples -name '*.py' | xargs` --count --select=E9,F63,F7,F82,E225,E251 --show-source --statistics
@@ -67,7 +67,7 @@ pip wheel --verbose --no-deps --no-clean dist/optax*.tar.gz
 pip install optax*.whl
 
 # Check types with pytype.
-pip install pytype
+pip install -q pytype
 pytype `find optax/_src examples optax/contrib -name '*.py' | xargs` -k -d import-error
 
 # Run tests using pytest.
@@ -77,7 +77,7 @@ python -m pytest -n auto --pyargs optax
 cd ..
 
 # Build Sphinx docs.
-pip install -e ".[docs]"
+pip install -q -e ".[docs]"
 cd docs && make html
 # run doctests
 make doctest
