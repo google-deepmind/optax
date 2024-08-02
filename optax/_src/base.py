cf01e76b16e9a7959f55fe70c18d31aad875c031
Gilad Turok <36947659+gil2rok@users.noreply.github.com>
2024-08-02 00:44:40 +0000

    doc emphasis in rst format

--- optax/_src/base.py
+++ optax/_src/base.py
@@ -153,7 +153,7 @@ class GradientTransformation(NamedTuple):
   # pylint: disable=line-too-long
   """A pair of pure functions implementing a gradient transformation.
 
-  Optax optimizers are all implemented as _gradient transformations_.
+  Optax optimizers are all implemented as *gradient transformations*.
   A gradient transformation is defined to be a pair of pure functions, which
   are combined together in a `NamedTuple` so that they can be referred to by
   name.
@@ -165,7 +165,7 @@ class GradientTransformation(NamedTuple):
   Since gradient transformations do not contain any internal state, all stateful
   optimizer properties (such as the current step count when using optimizer
   schedules or  momentum values) are passed through optax gradient
-  transformations by using the optimizer _state_ pytree. Each time a gradient
+  transformations by using the optimizer *state* pytree. Each time a gradient
   transformation is applied, a new state is computed and returned, ready to be
   passed to the next call to the gradient transformation.
 
