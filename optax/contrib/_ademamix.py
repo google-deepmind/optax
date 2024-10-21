4b621aaca6561ba0d7315916f1f33677fe0a181e
Daniel Marthaler <daniel.marthaler@stitchfix.com>
2024-10-21 16:26:35 -0400

    added docstrings and matched adamw api

--- optax/contrib/_ademamix.py
+++ optax/contrib/_ademamix.py
@@ -15,18 +15,23 @@ from optax._src import numerics
 from optax._src import transform
 import optax.tree_utils as otu
 from jax.lax import rsqrt
-from typing import NamedTuple, Optional, Tuple
+from typing import NamedTuple, Tuple
 
 
-def alpha_scheduler(alpha, alpha_start: float = 0, T_alpha: int = 0) -> base.Schedule:
+def alpha_scheduler(
+  alpha_value,
+  alpha_start: float = 0,
+  warmup_alpha: int = 0
+) -> base.Schedule:
   """The alpha scheduler from the paper.
 
   This is a progressive increase in alpha using a linear scheduler.
 
   Args:
-    alpha: The current value of alpha (the coefficient that "blends" the two EMAs)
+    alpha_value: The current value of alpha (the coefficient that "blends" 
+      the two EMAs)
     alpha_start: The starting value of alpha
-    T_alpha: The warmup time for alpha to reach it's final value.
+    warmup_alpha: The warmup time for alpha to reach it's final value.
 
   Returns:
     A `base.Schedule` object.
@@ -34,60 +39,75 @@ def alpha_scheduler(alpha, alpha_start: float = 0, T_alpha: int = 0) -> base.Sch
   """
 
   def schedule(step: int) -> float:
-    is_warmup: float = (step < T_alpha).astype(jnp.float32)
-    a: float = step / float(T_alpha)
-    return is_warmup * ((1.0 - a) * alpha_start + a * alpha) + alpha * (1.0 - is_warmup)
-
+    is_warmup: float = jnp.array(step < warmup_alpha).astype(jnp.float32)
+    a: float = step / float(warmup_alpha)
+    return (
+      is_warmup * ((1.0 - a) * alpha_start + a * alpha_value) +
+      alpha_value * (1.0 - is_warmup)
+    )
   return schedule
 
 
-def b3_scheduler(beta_end: float, beta_start: float = 0, T_b3: int = 0):
+def b3_scheduler(
+  beta_end: float,
+  beta_start: float = 0,
+  warmup_b3: int = 0
+) -> base.Schedule:
   """The b3 scheduler from the paper.
 
-  This is a progressive increase in b3 attempting to increase t_half linearly
+  This is a progressive increase in b3 attempting to increase the number
+  of iterations corresponding to where half of the mass of the second EMA
+  is concentrated (denoted ``t_half`` in the paper).  This scheduler attempts
+  to increase this value linearly.  Note for ``b3 = 0.9999, t_half`` is
+  approximately ``6930.``
   (Appendix A.1 of the paper derives the scheduler.)
 
   Args:
-    beta_end: The current value of b3 (the exponential decay rate to track the
-      first moment of past gradients for the second EMA)
+    beta_end: The desired ending value of b3 (the exponential decay rate to 
+      track the first moment of past gradients for the second EMA)
     beta_start: The starting value of b3
-    T_b3: The warmup time for b3 to reach it's maximal value.
+    warmup_b3: The warmup time for b3 to reach it's maximal value.
 
   Returns:
     A `base.Schedule` object.
 
   """
 
-  def f(beta: float) -> float:
+  def fun(beta: float) -> float:
     return jnp.log(0.5) / jnp.log(beta) - 1
 
   def f_inv(t: float) -> float:
     return rsqrt(t + 1)
 
   def schedule(step: int) -> float:
-    is_warmup = (step < T_b3).astype(jnp.float32)
-    alpha = step / float(T_b3)
-    return is_warmup * f_inv((1.0 - alpha) * f(beta_start) + alpha * f(beta_end)) + beta_end * (1.0 - is_warmup)
-
+    is_warmup = jnp.array(step < warmup_b3).astype(jnp.float32)
+    step_over_warmup = step / float(warmup_b3)
+    return (
+      is_warmup * f_inv((1.0 - step_over_warmup) * fun(beta_start)
+      + step_over_warmup * fun(beta_end))
+      + beta_end * (1.0 - is_warmup)
+    )
   return schedule
 
 
 class ScaleByAdemamixState(NamedTuple):
-   """State for the Ademamix algorithm."""
+  """State for the Ademamix algorithm."""
 
-   count: chex.Array  # shape=(), dtype=jnp.int32.
-   count_m2: chex.Array  # shape=(), dtype=jnp.int32.
-   m1: base.Updates
-   m2: base.Updates
-   nu: base.Updates
+  count: chex.Array  # shape=(), dtype=jnp.int32.
+  count_m2: chex.Array  # shape=(), dtype=jnp.int32.
+  m1: base.Updates
+  m2: base.Updates
+  nu: base.Updates
 
 
 def scale_by_ademamix(
-  b1: float = 0.9,
-  b2: float = 0.999,
-  b3: base.ScalarOrSchedule = 0.9999,
-  alpha: base.ScalarOrSchedule = 5.0,
+  b1: float,
+  b2: float,
+  b3: base.ScalarOrSchedule,
+  alpha: base.ScalarOrSchedule,
   eps: float = 1e-8,
+  eps_root: float = 0.0,
+  weight_decay: float=0.0,
 ) -> base.GradientTransformation:
   """Rescale updates according to the Ademamix algorithm.
 
@@ -105,18 +125,14 @@ def scale_by_ademamix(
       :math:`[4,10]` work well in practice.
     eps: A small constant applied to denominator outside of the square root
       (as in the Adam paper) to avoid dividing by zero when rescaling.
+    eps_root: Term added to the denominator inside of the square-root to improve
+      numerical stability when backpropagating gradients through the rescaling.
+    weight_decay: A small constant applied to denominator outside of the square root
+      (as in the Adam paper) to avoid dividing by zero when rescaling.
 
   Returns:
     A `GradientTransformation` object.
 
-  Limitations: AdEMAMix consists in leveraging very old gradients. Therefore,
-    the method is best suited to settings where the number of iterations is
-    important. The paper reports on this effect in App. C.1.5, showing how
-    smaller values of b3 (e.g. b3 = 0.999) can be better for low iterations
-    scenarios. Moreover, retaining gradient information over many thousands
-    steps can pose a problem in domains requiring fast adaptation to a sudden
-    distribution shift, or general cases in which the distribution is
-    non-stationary.
   """
 
   def init_fn(params):
@@ -135,9 +151,9 @@ def scale_by_ademamix(
     updates: jtu.tree_map, state, params=None
   ) -> Tuple[jtu.tree_map, ScaleByAdemamixState]:
     del params
-    c_b3 = b3_scheduler(state.count_m2) if callable(b3_scheduler) else b3
+    c_b3 = b3_scheduler(state.count_m2) if callable(b3) else b3
     c_alpha = (
-      alpha_scheduler(state.count_m2) if callable(alpha_scheduler) else alpha
+      alpha_scheduler(state.count_m2) if callable(alpha) else alpha
     )
     m1 = otu.tree_update_moment(
       updates, state.m1, b1, order=1
@@ -170,89 +186,108 @@ def ademamix(
   b3: base.ScalarOrSchedule = 0.9999,
   alpha: base.ScalarOrSchedule = 5.0,
   eps: float = 1e-8,
+  eps_root: float = 0.0,
   weight_decay: float = 0.0,
 ) -> base.GradientTransformation:
-  """The Ademamix optimizer.
+r"""AdEMAMix.
 
-  Description
+  AdEMAMix (Adaptive EMA Mixture) is AdamW with a mixture of two momentum 
+  terms to better take advantage of historical gradients. 
 
-  Examples:
-    > import optax
-    > import jax
-    > import jax.numpy as jnp
-    > def f(x): return jnp.sum(x ** 2)  # simple quadratic functio
-    > solver = optax.ademamix(learning_rate=0.003)
-    > params = jnp.array([1., 2., 3.])
-    > print('Objective function: ', f(params))
-      Objective function:  14.0
-    > opt_state = solver.init(params
-    > for _ in range(5):
-      ...  grad = jax.grad(f)(params)
-      ...  updates, opt_state = solver.update(grad, opt_state, params)
-      ...  params = optax.apply_updates(params, updates)
-      ...  print('Objective function: {:.2E}'.format(f(params)))
-      Objective function: 1.40E+01
-      Objective function: 1.39E+01
-      Objective function: 1.39E+01
-      Objective function: 1.39E+01
-      Objective function: 1.38E+01
+  Both SGD with momemtum (SGD+M) and Adam incorporate momentum using
+  Exponential Moving Averages (EMAs) of past gradients
 
-  References:
-    Pagliardini et al, 2024: https://arxiv.org/pdf/2409.03137
+  Let :math:`\eta` represent the learning rate and :math:`\beta_1, \beta_2`,
+  :math:`\beta_3, \alpha, \varepsilon, \bar{\varepsilon}`, represent the 
+  arguments ``b1``, ``b2``, ``b3``, ``alpha``, ``eps``  and ``eps_root``
+  respectively. Let :math:`\lambda` be the weight decay and :math:`\theta_t` 
+  the parameter vector at time :math:`t`.
 
-  Args:
-    b1: Exponential decay rate to track the first moment of past gradients for
-      the first Exponential Moving Average (EMA) - same as AdamW
-    b2: Exponential decay rate to track the second moment of past gradients for
-      the first Exponential Moving Average (EMA) - same as AdamW
-    b3: Exponential decay rate to track the first moment of past gradients
-      for the second EMA.
-    alpha: the coefficient that "blends" the two EMAs. paper states values in
-         :math:`[4,10]` work well in practice.
-    eps: A small constant applied to denominator outside of the square root
-      (as in the Adam paper) to avoid dividing by zero when rescaling.
-    weight_decay: Strength of the weight decay regularization.
+  The ``init`` function of this optimizer initializes an internal state
+  :math:`S_0 := (m1_0, m2_0, v_0) = (0, 0, 0)`, representing initial estimates 
+  for the first and second moments. In practice these values are stored as pytrees
+  containing all zeros, with the same shape as the model updates.
+  At step :math:`t`, the ``update`` function of this optimizer takes as
+  arguments the incoming gradients :math:`g_t`, the optimizer state :math:`S_t` 
+  and the parameters :math:`\theta_t` and computes updates :math:`\theta_{t+1}` and 
+  new state :math:`S_{t+1}`. Thus, for :math:`t > 0`, we have,
 
-  Returns:
-    A `GradientTransformation` object.
+  .. math::
+
+    \begin{align*}
+      m1_t &\leftarrow \beta_1 \cdot m1_{t-1} + (1-\beta_1) \cdot g_t \\
+      m2_t &\leftarrow \beta_3 \cdot m2_{t-1} + (1-\beta_3) \cdot g_t \\
+      v_t &\leftarrow \beta_2 \cdot v_{t-1} + (1-\beta_2) \cdot {g_t}^2 \\
+      \hat{m}_t &\leftarrow m_t / {(1-\beta_1^t)} \\
+      \hat{v}_t &\leftarrow v_t / {(1-\beta_2^t)} \\
+      \theta_t &\leftarrow \theta_{t-1} - \eta \cdot \left( (\hat{m1}_t +
+      \alpha m2_t) / \left({\sqrt{\hat{v}_t + \bar{\varepsilon}} + \varepsilon\right) 
+      + \lambda \theta_{t-1} \right).\\
+      S_t &\leftarrow (m1_t, m2_t, v_t).
+    \end{align*}
 
   Limitations: AdEMAMix consists in leveraging very old gradients. Therefore,
     the method is best suited to settings where the number of iterations is
-    important. The paper reports on this effect in App. C.1.5, showing how
+    important. The paper reports on this effect in Appendix C.1.5, showing how
     smaller values of b3 (e.g. b3 = 0.999) can be better for low iterations
     scenarios. Moreover, retaining gradient information over many thousands of
     steps can pose a problem in domains requiring fast adaptation to a sudden
     distribution shift, or general cases in which the distribution is
     non-stationary.
+
+  Examples:
+    >>> import optax
+    >>> import jax
+    >>> import jax.numpy as jnp
+    >>> def f(x): return jnp.sum(x ** 2)  # simple quadratic function
+    >>> solver = optax.adamw(learning_rate=0.003)
+    >>> params = jnp.array([1., 2., 3.])
+    >>> print('Objective function: ', f(params))
+    Objective function:  14.0
+    >>> opt_state = solver.init(params)
+    >>> for _ in range(5):
+    ...  grad = jax.grad(f)(params)
+    ...  updates, opt_state = solver.update(grad, opt_state, params)
+    ...  params = optax.apply_updates(params, updates)
+    ...  print('Objective function: {:.2E}'.format(f(params)))
+    Objective function: 1.40E+01
+    Objective function: 1.39E+01
+    Objective function: 1.39E+01
+    Objective function: 1.39E+01
+    Objective function: 1.38E+01
+
+  References:
+    "THE ADEMAMIX OPTIMIZER: BETTER, FASTER, OLDER"
+    (https://arxiv.org/pdf/2409.03137) by Matteo Pagliardini, 
+    Pierre Ablin and David Grangier.
+
+  Args:
+    learning_rate: A global scaling factor, either fixed or evolving along
+      iterations with a scheduler, see :func:`optax.scale_by_learning_rate`.
+    b1: Exponential decay rate to track the fast EMA.
+    b2: Exponential decay rate to track the second moment of past gradients.
+    b3: Exponenital decay rate to track the slow EMA.
+    alpha: Mixing coefficient in the linear combination fo the fast and slow EMAs. 
+    eps: A small constant applied to denominator outside of the square root
+      (as in the Adam paper) to avoid dividing by zero when rescaling.
+    eps_root: A small constant applied to denominator inside the square root (as
+      in RMSProp), to avoid dividing by zero when rescaling. This is needed for
+      instance when computing (meta-)gradients through Adam.
+    weight_decay: Strength of the weight decay regularization. Note that this
+      weight decay is multiplied with the learning rate. This is consistent
+      with other frameworks such as PyTorch, but different from
+      (Loshchilov et al, 2019) where the weight decay is only multiplied with
+      the "schedule multiplier", but not the base learning rate.
+
+  Returns:
+    The corresponding `GradientTransformation`.
+
+  .. seealso::
+    See the related functions :func:`optax.adam`, :func:`optax.nadamw`, as well
+    as the example :doc:`../_collections/examples/contrib/rosenbrock_ademamix` for a use case.
   """
   return combine.chain(
-    scale_by_ademamix(b1, b2, b3, alpha, eps),
+    scale_by_ademamix(b1, b2, b3, alpha, eps, eps_root),
       transform.add_decayed_weights(weight_decay),
       transform.scale_by_learning_rate(learning_rate),
   )
-
-
-if __name__ == "__main__":  # dummy test
-  import jax
-  import jax.numpy as jnp
-  def f(x):
-    return jnp.sum(x**2)  # simple quadratic function
-
-  alpha = 8.0
-  b1, b2, b3 = 0.9, 0.999, 0.9999
-
-  f_a = alpha_scheduler(alpha, alpha_start=0, T_alpha=10)
-  f_b3 = b3_scheduler(b3, beta_start=b1, T_b3=10)
-
-  solver = ademamix(learning_rate=0.01, b1=b1, b2=b2, b3=f_b3, alpha=f_a, weight_decay=0.01)
-
-  params = jnp.array([1.0, 2.0, 3.0])
-  print("Objective function: {:.2f}".format(f(params)))
-  opt_state = solver.init(params)
-  for itr in range(100):
-    grad = jax.grad(f)(params)
-    updates, opt_state = solver.update(grad, opt_state, params)
-    params = jax.tree_util.tree_map(lambda p, u: p + u, params, updates)
-    if itr % 5 == 0:
-      print("Objective function: {:.2f}".format(f(params)))
-  print(params)
