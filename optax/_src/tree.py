1c4215f2d69bc8e80a98f49d83131e39aaa69e28
Ata Shaker <ata.shaker@tum.de>
2025-01-03 18:21:05 +0100

    Move 'optax.tree' to '_src'

--- optax/_src/tree.py
+++ optax/_src/tree.py
@@ -1,7 +1,10 @@
+# pylint: disable=line-too-long
+# pylint: disable=redefined-builtin
 """Utilities for working with tree-like container data structures.
 
 The :mod:`optax.tree` namespace contains aliases of utilities from :mod:`optax.tree_util`.
 """
+# pylint: disable=unused-import
 from optax.tree_utils._casting import (
 tree_cast as cast,
 tree_dtype as dtype,
@@ -12,7 +15,7 @@ from optax.tree_utils._random import (
 )
 
 from optax.tree_utils._state_utils import (
-    NamedTupleKey as NamedTupleKey,
+    NamedTupleKey,
     tree_get as get,
     tree_get_all_with_path as get_all_with_path,
     tree_map_params as map_params,
@@ -41,6 +44,5 @@ from optax.tree_utils._tree_math import (
     tree_update_moment_per_elem_norm as update_moment_per_elem_norm,
     tree_vdot as vdot,
     tree_where as where,
-    tree_zeros_like as zeros_like,
+    tree_zeros_like as zeros_like
 )
-
