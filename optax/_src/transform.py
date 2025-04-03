33eda7387a4b57c570f49dd8f5804fc6c816ec44
tinker495 <wjdrbtjr495@gmail.com>
2025-04-03 18:59:46 +0900

    abstract

--- optax/_src/transform.py
+++ optax/_src/transform.py
@@ -307,6 +307,70 @@ def scale_by_adam(
   return base.GradientTransformation(init_fn, update_fn)
 
 
+def scale_by_adopt(
+    b1: float = 0.9,
+    b2: float = 0.999,
+    eps: float = 1e-8,
+    eps_root: float = 0.0,
+    mu_dtype: Optional[chex.ArrayDType] = None,
+    *,
+    nesterov: bool = False,
+    use_clipping: bool = False,
+) -> base.GradientTransformation:
+  r"""Rescale updates according to the Adam algorithm.
+
+  See :func:`optax.adam` for more details.
+
+  Args:
+    b1: Decay rate for the exponentially weighted average of grads.
+    b2: Decay rate for the exponentially weighted average of squared grads.
+    eps: Term added to the denominator to improve numerical stability.
+    eps_root: Term added to the denominator inside the square-root to improve
+      numerical stability when backpropagating gradients through the rescaling.
+    mu_dtype: Optional `dtype` to be used for the first order accumulator; if
+      `None` then the `dtype` is inferred from `params` and `updates`.
+    nesterov: Whether to use Nesterov momentum. The variant of Adam with
+      Nesterov momentum is described in [Dozat 2016]
+
+  Returns:
+    A :class:`optax.GradientTransformation` object.
+  """
+
+  mu_dtype = utils.canonicalize_dtype(mu_dtype)
+
+  def init_fn(params):
+    mu = otu.tree_zeros_like(params, dtype=mu_dtype)  # First moment
+    nu = otu.tree_zeros_like(params)  # Second moment
+    return ScaleByAdamState(count=jnp.zeros([], jnp.int32), mu=mu, nu=nu)
+
+  def update_fn(updates, state, params=None):
+    del params
+    mu = otu.tree_update_moment(updates, state.mu, b1, 1)
+    nu = otu.tree_update_moment_per_elem_norm(updates, state.nu, b2, 2)
+    count_inc = numerics.safe_increment(state.count)
+    if nesterov:
+      mu_hat = jax.tree.map(
+          lambda m, g: b1 * m + (1 - b1) * g,
+          otu.tree_bias_correction(mu, b1, numerics.safe_increment(count_inc)),
+          otu.tree_bias_correction(updates, b1, count_inc),
+      )
+    else:
+      mu_hat = otu.tree_bias_correction(mu, b1, count_inc)
+    # Dozat 2016 https://openreview.net/pdf?id=OM0jvwB8jIp57ZJjtNEZ
+    # Algorithm 2 further multiplies Adam's standard nu_hat by b2. It is
+    # unclear why. Other Nadam implementations also omit the extra b2 factor.
+    nu_hat = otu.tree_bias_correction(nu, b2, count_inc)
+    updates = jax.tree.map(
+        lambda m, v: None if m is None else m / (jnp.sqrt(v + eps_root) + eps),
+        mu_hat,
+        nu_hat,
+        is_leaf=lambda x: x is None,
+    )
+    mu = otu.tree_cast(mu, mu_dtype)
+    return updates, ScaleByAdamState(count=count_inc, mu=mu, nu=nu)
+
+  return base.GradientTransformation(init_fn, update_fn)
+
 class ScaleByAmsgradState(NamedTuple):
   """State for the AMSGrad algorithm."""
 
