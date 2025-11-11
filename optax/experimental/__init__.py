1405607998c21f1b3fe744b5a185a230d4317fd2
Matteo Hessel <mtthss@google.com>
2025-11-11 05:31:41 -0800

    fix init
    PiperOrigin-RevId: 830869505

--- optax/experimental/__init__.py
+++ optax/experimental/__init__.py
@@ -15,6 +15,6 @@
 
 """Experimental optax modules."""
 
-from . import _aggregating as aggregating
-from . import _microbatching as microbatching
-from ._microbatching import microbatch  # pylint: disable=g-importing-member
+from optax.experimental import _aggregating as aggregating
+from optax.experimental import _microbatching as microbatching
+microbatch = microbatching.microbatch
