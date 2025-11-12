2f5ce7b5c57d333eeb59be20fa923b1828ed46a3
Vincent Roulet <vroulet@google.com>
2025-11-11 16:59:24 -0800

    Add an internal definition of `ArrayTree` and use it instead of `chex.ArrayTree`.
    PiperOrigin-RevId: 831123754

--- optax/transforms/_clipping.py
+++ optax/transforms/_clipping.py
@@ -20,7 +20,6 @@ https://gist.github.com/wdphy16/118aef6fb5f82c49790d7678cf87da29
 
 from typing import Optional, Union
 
-import chex
 import jax
 import jax.numpy as jnp
 from optax._src import base
@@ -111,7 +110,7 @@ def clip_by_global_norm(
   return base.GradientTransformation(base.init_empty_state, update_fn)
 
 
-def _check_arrays_have_batch_dim(grads: chex.ArrayTree) -> bool:
+def _check_arrays_have_batch_dim(grads: base.ArrayTree) -> bool:
   """Checks that each array in grads has a batch dimension in the 0th axis."""
   grads = jax.tree.flatten(grads)[0]
   batch_size = grads[0].shape[0]
@@ -119,8 +118,8 @@ def _check_arrays_have_batch_dim(grads: chex.ArrayTree) -> bool:
 
 
 def per_example_global_norm_clip(
-    grads: chex.ArrayTree, l2_norm_clip: jax.typing.ArrayLike  # float
-) -> tuple[chex.ArrayTree, jax.Array]:
+    grads: base.ArrayTree, l2_norm_clip: jax.typing.ArrayLike  # float
+) -> tuple[base.ArrayTree, jax.Array]:
   """Applies gradient clipping per-example using their global norm.
 
   Args:
@@ -170,10 +169,10 @@ def per_example_global_norm_clip(
 
 
 def per_example_layer_norm_clip(
-    grads: chex.ArrayTree,
+    grads: base.ArrayTree,
     global_l2_norm_clip: jax.typing.ArrayLike,  # float
     uniform: bool = True
-) -> tuple[chex.ArrayTree, chex.ArrayTree]:
+) -> tuple[base.ArrayTree, base.ArrayTree]:
   """Applies gradient clipping per-example using per-layer norms.
 
   If len(grads) == 1, this function is equivalent to
