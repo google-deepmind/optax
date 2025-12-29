a194389e29bc3bea2cdb0ab19158933e8ebd807b
Robert Dyro <rdyro@google.com>
2025-12-29 13:26:21 -0800

    fix failing CI
    PiperOrigin-RevId: 850145283

--- optax/_src/transform_test.py
+++ optax/_src/transform_test.py
@@ -180,7 +180,9 @@ class TransformTest(parameterized.TestCase):
       opt = transform.scale_by_lion(
           b1=b1, b2=0.9, mode=mode, smooth_beta=smooth_beta
       )
-      state: transform.ScaleByLionState = opt.init(updates)  # pytype: disable=annotation-type-mismatch
+      state: transform.ScaleByLionState = opt.init(
+          updates
+      )  # pytype: disable=annotation-type-mismatch
       out_updates, _ = opt.update(updates, state)
 
       x = (1.0 - b1) * updates + b1 * state.mu
@@ -196,7 +198,9 @@ class TransformTest(parameterized.TestCase):
 
   def test_lion_invalid_mode_raises(self):
     updates = jnp.array([0.1, -0.2])
-    opt = transform.scale_by_lion(mode='invalid_mode')  # pytype: disable=wrong-arg-types
+    opt = transform.scale_by_lion(
+        mode='invalid_mode'
+    )  # pytype: disable=wrong-arg-types
     state = opt.init(updates)
     with self.assertRaises(ValueError):
       opt.update(updates, state)
